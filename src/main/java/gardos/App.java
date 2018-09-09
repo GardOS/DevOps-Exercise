3896d419d9eab75edf4bd88571cd2ed7165f8450
GardOS <gardo1506@gmail.com>
2018-09-09 14:32:10 +0200

    Add hello-world project

---
+++ src/main/java/gardos/App.java
@@ -0,0 +1,9 @@
+package gardos;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/gardos/AppTest.java
@@ -0,0 +1,21 @@
+package gardos;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest
+{
+    @Test
+    public void assertIsTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void assertIsFalse()
+    {
+        assertFalse( false );
+    }
+}
